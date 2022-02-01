c11b39231c49411c1b713824bd0cf531be147eff
ShravanBhat <2017.shravan.bhat@ves.ac.in>
2022-02-01 08:28:59 +0530

    final changes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 redis = {
   host: process.env.REDIS_HOST || 'redis-10901.c92.us-east-1-3.ec2.cloud.redislabs.com',
   port: process.env.REDIS_PORT || 10901,
-  password: process.env.REDIS_PASSWORD || 'iPeSzISqXJsgXob06dITZfWuOHZcSuy0'
+  password: process.env.REDIS_PASSWORD || 'Enter the password here'
 }
 module.exports = redis;
